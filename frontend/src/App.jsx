39ffab9c9dd427466eab8bfb57e72b0f8b0e6a44
JatinShimpi <shimpijatin184@gmail.com>
2025-02-17 18:50:36 +0530

    initialize frontend and backend structure with React, Vite, Tailwind CSS and node.js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/NavBar";
+import MainPage from "./components/MainPage";
+
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen">
+      <Navbar />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
